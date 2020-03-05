e83da1aae8f8d3425b7d71633043aebbd5d0281e
陈达寅 <11612512@mail.sustc.edu.cn>
2020-03-04 20:36:48 -0800

    Update insert_test.cc

--- insert_test.cc
+++ insert_test.cc
@@ -13,12 +13,16 @@ main (int argc, char* argv[])
 	unsigned seed;
 	seed = time(0);
 	srand(seed);
+	int number = rand() % 100001;
+	printf("number is %d\n", number);
 	int nprobes_total = 0;
 	double load = 0;
 	int cnt = 0;
 	while(cnt<5){
+		
 
-		int number = rand() % 100001;
+		
+		printf("%Lg\n", nprobes_total/cnt);
 		nprobes_total = nprobes_total + hashTable.insert(number);
 		cnt++;
 		load = hashTable.load_factor();
